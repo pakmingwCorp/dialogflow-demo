7f1c627383614a2b0702dc57cb61a4b259613958
Pak-Ming Wan <pakmingw@google.com>
2018-11-21 00:23:59 +0100

    removed project specifics from deployment script

--- webClient/chatbotApp/build_and_deploy.sh
+++ webClient/chatbotApp/build_and_deploy.sh
@@ -9,8 +9,8 @@ echo "Copying Google App Engine app.yaml to deployment location"
 cp app.yaml dist/chatbotApp/
 echo "Deploying to Google App Engine"
 # setup gcloud parameters
-export PROJECT_ID="dialogflow-demo-215609"
-export GCP_REGION="us-central1-f"
+export PROJECT_ID="PROJECT_ID"
+export GCP_REGION="GCP_REGION"
 # gcloud config list project
 gcloud config set project $PROJECT_ID
 gcloud config set compute/zone $GCP_REGION
